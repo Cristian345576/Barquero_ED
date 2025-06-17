7055c46ace14b60ce8a58dec9c2d4004de4b6279
Barquero Rodríguez <cristian.barquero@isepceu.es>
2025-06-17 15:38:41 +0200

    Segunda clase Rectangulo Cristian

---
+++ src/main/java/formas/Barquero_Rectangulo.java
@@ -0,0 +1,96 @@
+package formas;
+
+public class Barquero_Rectangulo {
+	private int Id;
+	private double LadoA;
+	private double LadoB;
+	private String Color;
+	
+	
+	
+public Barquero_Rectangulo() {
+		super();
+	}
+	
+	
+
+	public Barquero_Rectangulo(int id, double ladoA, double ladoB, String color) {
+		super();
+		Id = id;
+		LadoA = ladoA;
+		LadoB = ladoB;
+		Color = color;
+	}
+
+
+
+	public Barquero_Rectangulo(double ladoA, double ladoB, String color) {
+		super();
+		LadoA = ladoA;
+		LadoB = ladoB;
+		Color = color;
+	}
+
+
+
+	public int getId() {
+		return Id;
+	}
+
+
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+
+
+	public double getLadoA() {
+		return LadoA;
+	}
+
+
+
+	public void setLadoA(double ladoA) {
+		LadoA = ladoA;
+	}
+
+
+
+	public double getLadoB() {
+		return LadoB;
+	}
+
+
+
+	public void setLadoB(double ladoB) {
+		LadoB = ladoB;
+	}
+
+
+
+	public String getColor() {
+		return Color;
+	}
+
+
+
+	public void setColor(String color) {
+		Color = color;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Barquero_Rectangulo [Id=" + Id + ", LadoA=" + LadoA + ", LadoB=" + LadoB + ", Color=" + Color + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
